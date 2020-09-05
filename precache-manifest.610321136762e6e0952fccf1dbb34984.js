0b2091bede5a3d6275400d93959c77f912383062
eljoedeleon <deliseo316@gmail.com>
2020-09-06 00:04:50 +0800

    Updates

---
+++ precache-manifest.610321136762e6e0952fccf1dbb34984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79df26ad20d8824f44825fecf3c530f5",
+    "url": "/JSCalculator/index.html"
+  },
+  {
+    "revision": "c047ac67cc025f69637b",
+    "url": "/JSCalculator/static/css/main.0e98d45e.chunk.css"
+  },
+  {
+    "revision": "ffcdcacf65259a373e70",
+    "url": "/JSCalculator/static/js/2.2047ee5c.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/JSCalculator/static/js/2.2047ee5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c047ac67cc025f69637b",
+    "url": "/JSCalculator/static/js/main.28c766a6.chunk.js"
+  },
+  {
+    "revision": "790b18b6eb96230e5177",
+    "url": "/JSCalculator/static/js/runtime-main.f67a1643.js"
+  }
+]);
